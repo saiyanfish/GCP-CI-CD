4be2e44c8de5a70d918ff9155e5ccbe077ca3068
andyliu0225 <raygun3491@gmail.com>
2024-05-30 09:49:16 +0800

    gcp001

--- router/userRouter.cjs
+++ router/userRouter.cjs
@@ -1,8 +1,8 @@
 const express = require('express');
 const s3User = require('../controller/userController.cjs');
+const mysqlUser = require('../controller/mysqlUserController.cjs');
 
 const userRouter = express.Router();
 
 userRouter.post('/create', s3User.createUser);
-
 module.exports = userRouter;
